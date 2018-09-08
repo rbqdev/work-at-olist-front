2b0a848e190bd51e69f37fceeaa0f1384c25211b
Robson Braga de Queiroz <rbq.dev@gmail.com>
2018-09-07 22:11:41 -0300

    Update package.json and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,18 @@ module.exports = {
                     "css-loader", // translates CSS into CommonJS
                     "sass-loader" // compiles Sass to CSS, using Node Sass by default
                 ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     }
