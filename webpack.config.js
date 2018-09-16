7b5ca2d0d2f4d38bbeecb7b02fc3f6a041bde149
Robson Braga de Queiroz <rbq.dev@gmail.com>
2018-09-16 20:41:52 -0300

    Update Version 2.0.0 to use Web Components

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     mode: 'development',
     entry: {
-        app: './src/app.js'
+        app: './src/app-component.js'
     },
     output: {
         filename: 'build.js',
